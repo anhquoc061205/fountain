59793d47d8bdf4c73a05f75d67ef2211d7c0eb7e
=anhquoc061205 <=kanekyken918@gmail.com>
2025-04-29 08:21:25 +0700

    sroll to top

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function (){
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+})
